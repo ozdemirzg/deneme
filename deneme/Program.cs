302233600a44a7f6645c9507880a7abf62fb2b0e
ozdemirzg <94961649+ozdemirzg@users.noreply.github.com>
2022-02-01 14:49:13 +0300

    01,02,2022

---
+++ deneme/Program.cs
@@ -0,0 +1,42 @@
+﻿using System;
+
+namespace deneme
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            
+                 //veri yapıları bu ikisi
+                string deger;  
+                string sonDeger;
+
+                //bu üçü de algoritma
+                deger = Giris(); //string geliyor
+                sonDeger = Proses(deger); //string bekliyor string veriyor
+                Cikis(sonDeger); //string bekliyor
+        }
+            static string Giris()
+
+            {
+                string girisM;
+                
+                //Giriş İşlemleri
+                Console.Write("Sayıyı Girin: ");  //yanına yazdırdık sayıyı write ile.line olsa ltına yazacaktı.
+                girisM = Console.ReadLine();
+                return girisM;
+            }
+            static string Proses(string g)
+            {
+                //String işlemler
+                cikisM = "Giriş :  " + g;
+                Console.WriteLine(cikisM);
+            }
+            static void Cikis(string c)
+            {
+                //Çıkış işlemleri
+                Console.WriteLine(c);
+            }
+        
+    }
+}
